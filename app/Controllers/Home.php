5975f4c6d9e32cfe8e8ce3bbe9492df54baf3110
MrcSatrio <marcsatrioff@gmail.com>
2023-08-08 20:54:18 +0700

    Up CI

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+    public function dashboard(): string
+    {
+        return view('kepalakoperasi/dashboard');
+    }
+}
