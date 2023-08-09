236de5eb86f1eeeb09a8a5707c4c252a9cbf7c80
aivdd <rahmantoedmodo@gmail.com>
2023-08-09 11:57:58 +0700

    edit template index

--- app/Views/kepalakoperasi/sidebar/index.php
+++ app/Views/kepalakoperasi/sidebar/index.php
@@ -2,10 +2,10 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
+        <!-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        </div> -->
+        <div class="sidebar-brand-text mx-3">Nama Instansi</div>
     </a>
 
     <!-- Divider -->
