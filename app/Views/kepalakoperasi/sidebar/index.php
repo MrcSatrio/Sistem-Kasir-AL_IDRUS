9188303e257e7cd22b9e517fc1ff5fb8dcab585d
aivdd <davidyusufrahmanto@gmail.com>
2023-08-11 22:11:59 +0700

    adjust sidebar kepala koperasi

--- app/Views/kepalakoperasi/sidebar/index.php
+++ app/Views/kepalakoperasi/sidebar/index.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
         <!-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div> -->
@@ -13,7 +13,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?= base_url('koperasi/dashboard') ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -23,20 +23,26 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+        Kelola
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+            <i class="fas fa-tags"></i>
+            <span>Produk</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+                <h6 class="collapse-header">Manajemen Produk :</h6>
+                <a class="collapse-item" href="#">Produk List</a>
+                <a class="collapse-item" href="#">Tambah Produk</a>
+                <h6 class="collapse-header">Manajemen Brand :</h6>
+                <a class="collapse-item" href="#">Brand List</a>
+                <a class="collapse-item" href="#">Tambah Brand</a>
+                <h6 class="collapse-header">Manajemen Kategori :</h6>
+                <a class="collapse-item" href="#">Kategori List</a>
+                <a class="collapse-item" href="#">Tambah Kategori</a>
             </div>
         </div>
     </li>
@@ -44,16 +50,27 @@
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
+            <i class="fas fa-user-tie"></i>
+            <span>Pegawai</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
+                <h6 class="collapse-header">Manajemen Pegawai:</h6>
+                <a class="collapse-item" href="#">Pegawai Terdaftar</a>
+                <a class="collapse-item" href="#">Tambah Pegawai</a>
+            </div>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="far fa-id-card"></i>
+            <span>Member</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Manajemen Member:</h6>
+                <a class="collapse-item" href="#">Member Terdaftar</a>
+                <a class="collapse-item" href="#">Tambah Member</a>
             </div>
         </div>
     </li>
@@ -63,41 +80,34 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Addons
+        Laporan
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item -  -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
+            <span>30 Hari Terakhir</span></a>
     </li>
 
-    <!-- Nav Item - Tables -->
+    <!-- Nav Item -  -->
     <li class="nav-item">
-        <a class="nav-link" href="tables.html">
+        <a class="nav-link" href="#">
             <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
+            <span>14 Hari Terakhir</span></a>
+    </li>
+    <!-- Nav Item -  -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>7 Hari Terakhir</span></a>
+    </li>
+
+    <!-- Nav Item -  -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Hari Ini</span></a>
     </li>
 
     <!-- Divider -->
