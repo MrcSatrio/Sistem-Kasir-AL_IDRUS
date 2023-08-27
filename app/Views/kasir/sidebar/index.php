d1e1ec01a7c461e73e53803c0e97041cfec9ab97
MrcSatrio <marcsatrioff@gmail.com>
2023-08-27 23:53:06 +0700

    remove useless search form topbar

--- app/Views/kasir/sidebar/index.php
+++ app/Views/kasir/sidebar/index.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('kasir/dashboard') ?>">
         <!-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div> -->
@@ -13,7 +13,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?= base_url('kasir/dashboard') ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard Kasir</span></a>
     </li>
--- app/Views/kepalakoperasi/sidebar/index.php
+++ app/Views/kepalakoperasi/sidebar/index.php
@@ -1,11 +1,11 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('koperasi/dashboard') ?>">
         <!-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div> -->
-        <div class="sidebar-brand-text mx-3"><?= session('instansi') ?></div>
+        <div class="sidebar-brand-text mx-3">AL-IDRUS</div>
     </a>
 
     <!-- Divider -->
