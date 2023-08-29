e0fe68d1e1e06df6dfd2f4e108a3fe71f7a4efb8
fatah1707 <fatahahmad807@gmail.com>
2023-08-29 13:59:25 +0700

    fix dajjal eye

--- app/Views/kepalakoperasi/pegawai/form-create.php
+++ app/Views/kepalakoperasi/pegawai/form-create.php
@@ -24,27 +24,43 @@
         </div>
     </div>
     <div class="form-group row">
-        <label for="password" class="col-sm-2 col-form-label">Password</label>
-        <div class="col-sm-10">
+    <label for="password" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+        <div class="input-group">
             <input type="password" class="form-control <?php echo session('field_error.password') ? 'is-invalid' : ''; ?>" id="password" placeholder="Password" name="password">
-            <?php if (session()->has('field_error.password')) : ?>
-                <div class="invalid-feedback">
-                    <?php echo session('field_error.password'); ?>
-                </div>
-            <?php endif; ?>
+            <div class="input-group-append">
+                <span class="input-group-text toggle-password" toggle="#password">
+                    <i class="fa fa-eye field-icon"></i>
+                </span>
+            </div>
         </div>
+        <?php if (session()->has('field_error.password')) : ?>
+            <div class="invalid-feedback">
+                <?php echo session('field_error.password'); ?>
+            </div>
+        <?php endif; ?>
     </div>
-    <div class="form-group row">
-        <label for="confirm_password" class="col-sm-2 col-form-label">Confirm Password</label>
-        <div class="col-sm-10">
+</div>
+
+<div class="form-group row">
+    <label for="password_confirm" class="col-sm-2 col-form-label">Confirm Password</label>
+    <div class="col-sm-10">
+        <div class="input-group">
             <input type="password" class="form-control <?php echo session('field_error.password_confirm') ? 'is-invalid' : ''; ?>" id="password_confirm" placeholder="Confirm Password" name="password_confirm">
-            <?php if (session()->has('field_error.password_confirm')) : ?>
-                <div class="invalid-feedback">
-                    <?php echo session('field_error.password_confirm'); ?>
-                </div>
-            <?php endif; ?>
+            <div class="input-group-append">
+                <span class="input-group-text toggle-password" toggle="#password_confirm">
+                    <i class="fa fa-eye field-icon"></i>
+                </span>
+            </div>
         </div>
+        <?php if (session()->has('field_error.password_confirm')) : ?>
+            <div class="invalid-feedback">
+                <?php echo session('field_error.password_confirm'); ?>
+            </div>
+        <?php endif; ?>
     </div>
+</div>
+
     <div class="form-group row">
         <label for="nama_pegawai" class="col-sm-2 col-form-label">Nama Pegawai</label>
         <div class="col-sm-10">
@@ -82,3 +98,20 @@
     <button class="btn btn-primary" type="submit" id="save">Simpan Data Pegawai</button>
 
 </form>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const togglePasswordIcons = document.querySelectorAll(".toggle-password");
+
+        togglePasswordIcons.forEach(icon => {
+            icon.addEventListener("click", function() {
+                const target = document.querySelector(this.getAttribute("toggle"));
+                if (target.type === "password") {
+                    target.type = "text";
+                } else {
+                    target.type = "password";
+                }
+            });
+        });
+    });
+</script>
